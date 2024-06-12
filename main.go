6a55b023e8b5249c6ef75309fb4dbcafd4d5a6e5
JunYanBJSS <144220886+JunYanBJSS@users.noreply.github.com>
2024-06-12 13:31:56 +0100

    Graceful (#1)
    * Use Actor pattern
    
    * refactor graceful shutdown
    
    * refactor code
    
    * all done
    
    * all done
    
    ---------
    
    Co-authored-by: JunYanBJSS <johnnycocoyan@hotmail.com>

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+
 	fmt.Println("starting server")
 	server.NewServer()
 
--- server/server.go
+++ server/server.go
@@ -1,17 +1,27 @@
 package server
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"sync"
+	"os/signal"
+	"strconv"
+	"strings"
+	"time"
 )
 
-type MapStore struct {
-	peopleMap map[string]int
-	mu        sync.RWMutex
+var messageChan = make(chan message)
+var peopleMap = make(map[string]int)
+
+type message struct {
+	command  string
+	key      string
+	value    int
+	response chan<- string
 }
 
 type People struct {
@@ -19,94 +29,59 @@ type People struct {
 	Age  int    `json:"age"`
 }
 
-func (ms *MapStore) get() map[string]int {
-	ms.mu.RLock()
-	defer ms.mu.RUnlock()
-
-	return ms.peopleMap
-}
-
-func (ms *MapStore) set(key string, value int) {
-	ms.mu.Lock()
-	defer ms.mu.Unlock()
-
-	ms.peopleMap[key] = value
-}
-
-func (ms *MapStore) delete(key string) error {
-	ms.mu.Lock()
-	defer ms.mu.Unlock()
-	_, ok := ms.peopleMap[key]
-	if !ok {
-		return fmt.Errorf("people not found %s", key)
+func getPeople(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("getting people")
+	response := make(chan string)
+	messageChan <- message{command: "get", response: response}
+	_, err := fmt.Fprintln(w, <-response)
+	if err != nil {
+		return
 	}
-	delete(ms.peopleMap, key)
-	return nil
+}
+
+func createPeople(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("pareparing for creating people ...")
+	people := requestDecoder(w, r)
+	fmt.Println("createPeople")
+	response := make(chan string)
+	messageChan <- message{command: "put", key: people.Name, value: people.Age, response: response}
+	fmt.Fprintln(w, <-response)
+}
+
+func deletePeopleByName(w http.ResponseWriter, r *http.Request) {
+	key := r.PathValue("key")
+	res := make(chan string)
+	messageChan <- message{command: "delete", key: key, response: res}
+	fmt.Fprintln(w, <-res)
+
+}
+
+func initializeRouter() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /people", getPeople)
+	mux.HandleFunc("POST /people", createPeople)
+	mux.HandleFunc("DELETE /people/{key}", deletePeopleByName)
+	mux.HandleFunc("PUT /people", createPeople)
+	return mux
 }
 
 func NewServer() {
-	mapStore := &MapStore{peopleMap: make(map[string]int)}
-	server := &http.Server{Addr: ":8080"}
-	mapStore.peopleMap["yan"] = 25
-	mapStore.peopleMap["gin"] = 44
-	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case "GET":
-			getHandler(w, r, mapStore)
-		case "POST":
-			postHandler(w, r, mapStore)
-		case "PUT":
-			updateHandler(w, r, mapStore)
-		default:
-			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-		}
-	})
-	http.HandleFunc("/delete/", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Path[len("/delete/"):]
-		err := mapStore.delete(name)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
-	})
+	ctx, cancel := context.WithCancel(context.Background())
+	router := initializeRouter()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+	done := startServer(ctx, server)
 	terminate := make(chan os.Signal, 1)
-	go func() {
-		err := server.ListenAndServe()
-		if err != nil {
-			log.Fatal("server error: ", err)
-		}
-	}()
-
-	go func() {
-		<-terminate
-		err := server.Close()
-		if err != nil {
-			log.Fatal("server error: ", err)
-		}
-	}()
+	signal.Notify(terminate, os.Interrupt)
 
 	<-terminate
-}
+	log.Println("system closing---")
+	cancel()
+	fmt.Println("closing")
+	<-done
 
-func updateHandler(w http.ResponseWriter, r *http.Request, ms *MapStore) {
-	people := requestDecoder(w, r)
-	ms.set(people.Name, people.Age)
-	fmt.Fprintln(w, people.Name, ":", people.Age)
-}
-
-func postHandler(w http.ResponseWriter, r *http.Request, ms *MapStore) {
-	people := requestDecoder(w, r)
-	ms.set(people.Name, people.Age)
-	fmt.Fprintln(w, people.Name, ":", people.Age)
-}
-
-func getHandler(w http.ResponseWriter, r *http.Request, ms *MapStore) {
-	peoples := ms.get()
-	fmt.Fprintln(w, "{")
-	for k, v := range peoples {
-		fmt.Fprintf(w, "  %v: %v,\n", k, v)
-	}
-	fmt.Fprintln(w, "}")
 }
 
 func requestDecoder(w http.ResponseWriter, r *http.Request) People {
@@ -117,4 +92,72 @@ func requestDecoder(w http.ResponseWriter, r *http.Request) People {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 	return people
+}
+
+func run(ctx context.Context) <-chan struct{} {
+	done := make(chan struct{})
+	go func() {
+		<-ctx.Done()
+		close(messageChan)
+	}()
+	go func() {
+		defer close(done)
+
+		for mes := range messageChan {
+			switch mes.command {
+			case "get":
+				var str strings.Builder
+				str.WriteString("{")
+				for k, v := range peopleMap {
+					str.WriteString("\n" + k + ": " + strconv.Itoa(v))
+				}
+				str.WriteString("\n}")
+				mes.response <- str.String()
+			case "put":
+				peopleMap[mes.key] = mes.value
+				fmt.Println(mes.key, ": ", mes.value)
+				mes.response <- fmt.Sprintf("%s:%d", mes.key, mes.value)
+			case "delete":
+				delete(peopleMap, mes.key)
+				mes.response <- fmt.Sprintf("%s has deleted", mes.key)
+
+			}
+		}
+	}()
+	return done
+}
+
+func startServer(ctx context.Context, server *http.Server) <-chan struct{} {
+	var err error
+	done := make(chan struct{})
+	runDone := run(ctx)
+	go func() {
+		if err = server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Println(err)
+		}
+	}()
+	<-ctx.Done()
+	log.Println("server stopped")
+
+	go func() {
+		defer close(done)
+		<-ctx.Done()
+		if err := server.Shutdown(ctx); err != nil {
+			log.Println("Shutdown: " + err.Error())
+		}
+		<-runDone
+	}()
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() {
+		cancel()
+	}()
+	if err = server.Shutdown(ctxShutDown); err != nil {
+		log.Println(err)
+	}
+
+	if errors.Is(err, http.ErrServerClosed) {
+		err = nil
+	}
+	return done
+
 }
